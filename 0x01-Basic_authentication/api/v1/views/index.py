e120180a2424ad2a1ca0660206240c3c4fd080c8
Paul John <rainson.work@gmail.com>
2024-01-19 22:36:29 +0300

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized", methods=["GET"])
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized"""
     abort(401, "Unauthorized")
