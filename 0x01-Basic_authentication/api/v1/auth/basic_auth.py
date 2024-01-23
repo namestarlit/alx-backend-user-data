cd76ec138c7fab2fd648e6d8688421d93effc8eb
Paul John <rainson.work@gmail.com>
2024-01-23 13:28:45 +0300

    Update basic_auth module

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,17 @@ class BasicAuth(Auth):
             return None
 
         return b64_auth_header
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Extracts user credentials"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        user_credentials = decoded_base64_authorization_header.split(":", 1)
+        return (user_credentials[0], user_credentials[1])
