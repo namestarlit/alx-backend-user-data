faacc128536de7b8db088424a0dfedcb425f650f
Paul John <rainson.work@gmail.com>
2024-01-31 13:10:33 +0300

    Add user_session

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Database session module"""
 from models.base import Base
-from sqlalchemy import Column, Strin, ForeignKey
 
 
 class UserSession(Base):
     """UserSession class"""
 
-    __tablename__ = "user_sessions"
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    session_id = Column(String(60), nullable=False)
+    def __init__(self, *args: list, **kwargs: dict):
+        """Constructor"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
